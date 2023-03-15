02157a68078d9730561683a504e879fe4b833dcf
rijosh.easo <rijoshk@gmail.com>
2023-03-15 20:13:09 +1100

    second commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+def main():
+    # # download current stock list
+    get_stock_list()
+
+if __name__ == '__main__':
+    # print('pandas version: ', pd.__version__)
+    # print('yfinance version: ', yf.__version__)
+    main()
+
+
